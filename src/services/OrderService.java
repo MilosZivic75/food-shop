19fa5e482c61b15accc306626d025d70bb19ed09
vrba3 <vrbica.vlado11@gmail.com>
2021-07-08 00:45:49 +0200

    mala izmena nacina otkazivanja porudzbine

--- src/services/OrderService.java
+++ src/services/OrderService.java
@@ -2,11 +2,16 @@ package services;
 
 import beans.Order;
 import repositories.IRepository;
+import repositories.OrderRepository;
 
 public class OrderService extends Service<String, Order>{
-
+	private OrderRepository orderRepository = new OrderRepository();
+	
 	public OrderService(IRepository<String, Order> repository) {
 		super(repository);
 	}
-
+	
+	public void updateStatus(Order order) {
+		orderRepository.updateStatus(order);
+	}
 }
